7e086f526bf715534b83b7e303dddbc28958a067
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2021-09-15 18:16:52 -0300

    Comments added

--- visualization.py
+++ visualization.py
@@ -18,28 +18,36 @@ display.fill(pygame.Color("black"))
 
 
 def heapify(arr, arrLength, num):
-  root = num
-  left = 2 * num + 1
-  right = 2 * num + 2
+  root = num              # Inicializando um valor para ser a raiz
+  left = 2 * num + 1      # Filho da esquerda
+  right = 2 * num + 2     # Filho da direita
 
+  # Verificando se o filho da esquerda existe e se é maior que o seu elemento raiz
   if left < arrLength and arr[root] < arr[left]:
     root = left
 
+  # Verificando se o filho da direita existe e se é maior que o seu elemento raiz
   if right < arrLength and arr[root] < arr[right]:
     root = right
 
+  # caso algum dos elementos filhos seja maior, a troca de elementos raiz é feita
   if root != num:
     arr[num], arr[root] = arr[root], arr[num]
     update(arr[num], arr[root])
+
+    # Construindo um novo maxheap com as novas mudanças na árvore
     heapify(arr, arrLength, root)
   
 
+# A função heapsort organiza um array de tamanho 700
 def heapsort():  
   arrLength = len(arr)
 
+  # Constrói um maxheap
   for i in range(arrLength//2 - 1, -1, -1):
     heapify(arr, arrLength, i)
 
+  # Extraindo os elementos um por um
   for i in range(arrLength - 1, 0, -1):
     arr[i], arr[0] = arr[0], arr[i]
     heapify(arr, i, 0)
